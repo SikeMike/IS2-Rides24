9c411f6f3356acdbb6e26a78960d3f80954076bf
kroko8 <115846183+kroko8@users.noreply.github.com>
2024-10-02 16:15:54 +0200

    clase test

---
+++ src/test/java/gauzatuEragiketaTest/GauzatuEragiketaMockW.java
@@ -0,0 +1,99 @@
+package gauzatuEragiketaTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import dataAccess.DataAccess;
+import domain.Driver;
+import domain.Ride;
+import domain.User;
+import exceptions.RideAlreadyExistException;
+import exceptions.RideMustBeLaterThanTodayException;
+
+public class GauzatuEragiketaMockW {
+
+	static DataAccess sut;
+
+	protected MockedStatic<Persistence> persistenceMock;
+
+	@Mock
+	protected EntityManagerFactory entityManagerFactory;
+	@Mock
+	protected EntityManager db;
+	@Mock
+	protected EntityTransaction et;
+	@Mock
+    private TypedQuery<User> query;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.openMocks(this);
+		persistenceMock = Mockito.mockStatic(Persistence.class);
+		persistenceMock.when(() -> Persistence.createEntityManagerFactory(Mockito.any()))
+				.thenReturn(entityManagerFactory);
+
+		Mockito.doReturn(db).when(entityManagerFactory).createEntityManager();
+		Mockito.doReturn(et).when(db).getTransaction();
+		sut = new DataAccess(db);
+	}
+
+	@After
+	public void tearDown() {
+		persistenceMock.close();
+	}
+
+	User user;
+
+	@Test
+	public void test1() {
+	    try {
+	       
+	    	when(db.find(User.class, "username")).thenReturn(null);
+	    	user=null;
+	       
+	        boolean result = sut.gauzatuEragiketa("username", 100.0, true);
+
+	        // Verificar
+	        assertFalse(result);
+	        /*
+	        verify(et).begin();
+	        verify(et).commit();
+	        verify(et, never()).rollback();
+	        */
+	    } catch (NullPointerException e) {
+	        
+	        System.out.println("Error: NullPointerException atrapada.");
+	        e.printStackTrace();
+	        fail("NullPointerException fue lanzada cuando no debería haberlo sido.");
+	    }
+	}
+
+	
+	
+
+}
